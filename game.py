3967f15ca340c03921c02d5b7379103a25be35f8
maria8s <107110095+maria8s@users.noreply.github.com>
2023-04-08 19:23:27 +0500

    Update game.py

--- game.py
+++ game.py
@@ -34,9 +34,16 @@ mw.fill(BG_COLOR)
 display.set_caption('Пинг-Понг')
 clock = time.Clock()
 
-player_1 = Player('platform.png', 10, 200, 30, 90)
+player_1 = Player('platform.png', 10, 200, 30, 90, 5)
 player_2 = Player('platform.png', 560, 200, 30, 90, key_up=K_UP, key_down=K_DOWN)
+ball = Player('tenis_ball.png', 200, 200, 4, 50, 50)
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!!', True, (180, 0, 0))
 
+speed_x = 3
+speed_y = 3
 run = True
 while run:
     mw.fill(BG_COLOR)
@@ -48,6 +55,27 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-    
+
+       
+    player_1.update()
+    player_2.update()
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
+    if sprite.collide_rect(player_1, ball) or sprite.collide_rect(player_2, ball):
+        speed_x *= -1
+        speed_y *= 1
+
+    if ball.rect.y > 480-50 or ball.rect.y <0:
+        speed_y *= -1
+
+    if ball.rect.x < 0:
+        finish = True
+        mw.blit(lose1, (200, 200))
+        game_over = True
+    if ball.rect.x > 600:
+        finish = True
+        mw.blit(lose2, (200, 200))
+        game_over = True
+    ball.reset()
     display.update()
     clock.tick(FPS)
